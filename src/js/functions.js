35129a0afeffc6150b54b2f21a8a512fe0f824bf
gitcore33 <erickestanislau25@gmail.com>
2026-02-19 20:27:50 -0300

    starts to implement openlibrary api

--- src/js/functions.js
+++ src/js/functions.js
@@ -1,13 +1,25 @@
-export async function getBooks() {
-    let url = "https://www.googleapis.com/books/v1/volumes?q=subject:fiction&orderBy=newest&maxResults=40&key=AIzaSyCF6-ohKrgU1H_cx_Zmq7lk4CbPsKMvZkU"
+// export async function getBooks() {
+//     let url = "https://www.googleapis.com/books/v1/volumes?q=subject:fiction&orderBy=newest&maxResults=40&key=AIzaSyCF6-ohKrgU1H_cx_Zmq7lk4CbPsKMvZkU"
 
-    const data = await fetch(url);
+//     const data = await fetch(url);
 
-    const books = await data.json();
+//     const books = await data.json();
 
-    return books;
-}
+//     return books;
+// }
 
-export async function getCategories() {
+// export async function getCategories() {
     
+// }
+
+async function fetchTitle() {
+        const response = await fetch("https://openlibrary.org/search.json?q=has_fulltext:true&language:por&sort=new");
+        const data = await response.json();
+        // console.log(data.docs);
+
+        for(let book of data.docs) {
+            console.log(book.title);
+        }
 }
+
+fetchTitle();
